74b55a86981534a55fb404dcf56b089a5a5637d2
lahavana <36070872+lahavana@users.noreply.github.com>
2024-12-06 10:16:04 +0100

    Update 11_Language_Download.ps1
    new iso path for 24h2

--- 11_Language_Download.ps1
+++ 11_Language_Download.ps1
@@ -13,23 +13,8 @@
  write-host "The current path is $LocalPath"
  $LanguageShare = New-Item -Path $LocalPath -Name "LanguagesAndOptionalFeatures" -ItemType Directory -ErrorAction SilentlyContinue
 
-<#
-write-host "Start 1st download Azure"
-get-date
-Invoke-WebRequest -Uri "https://sawvdautimation.blob.core.windows.net/wvdoptimization/01_LANGUAGEPACK.iso" -OutFile "01_LANGUAGEPACK.iso"
-write-host "Finished 1st download Azure"
-write-host "Start 2nd download Azure"
-get-date
-Invoke-WebRequest -Uri "https://sawvdautimation.blob.core.windows.net/wvdoptimization/02_FODPACK.iso" -OutFile "02_FODPACK.iso"
-write-host "Finished 2nd download Azure"
-write-host "Start 3rd download Azure"
-get-date
-Invoke-WebRequest -Uri "https://sawvdautimation.blob.core.windows.net/wvdoptimization/03_InboxApps.iso" -OutFile "03_InboxApps.iso"
-write-host "Finished 3rd download Azure"
-#>
-
 write-host "Start download of language packs from Microsoft"
-Start-BitsTransfer -Source "https://software-download.microsoft.com/download/sg/22000.1.210604-1628.co_release_amd64fre_CLIENT_LOF_PACKAGES_OEM.iso" -Destination "01_LANGUAGEPACK.iso"
+Start-BitsTransfer -Source "https://software-static.download.prss.microsoft.com/dbazure/888969d5-f34g-4e03-ac9d-1f9786c66749/26100.1.240331-1435.ge_release_amd64fre_CLIENT_LOF_PACKAGES_OEM.iso" -Destination "01_LANGUAGEPACK.iso"
 Get-BitsTransfer
 
 write-host "Available Windows 10 1809 Languages and Features on Demand table."
@@ -53,19 +38,5 @@ foreach ($iso in $isos.name)
             }
     }
 
-
-
 write-host 'AIB Customization: Finished download languages script'
 
-
-<#
-#write-host "Start 1st download"
-#Invoke-WebRequest -Uri "https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_CLIENTLANGPACKDVD_OEM_MULTI.iso" -OutFile "01_LANGUAGEPACK.iso"
-#write-host "Finished 1st download"
-#write-host "Start 2nd download"
-#Invoke-WebRequest -Uri "https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_amd64fre_FOD-PACKAGES_OEM_PT1_amd64fre_MULTI.iso" -OutFile "02_FODPACK.iso"
-#write-host "Finished 2nd download"
-#write-host "Start 3rd download"
-#Invoke-WebRequest -Uri "https://software-download.microsoft.com/download/pr/19041.508.200905-1327.vb_release_svc_prod1_amd64fre_InboxApps.iso" -OutFile "03_InboxApps.iso"
-#write-host "Finished 3rd download"
-#>
