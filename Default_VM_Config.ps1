999d3ab26c4d53c0f44d5b83ea2eafc6a5f98b8f
lahavana <36070872+lahavana@users.noreply.github.com>
2025-10-13 13:39:28 +0200

    Update Default_VM_Config.ps1

--- Default_VM_Config.ps1
+++ Default_VM_Config.ps1
@@ -2,7 +2,7 @@
 ## Default VM Settings                                ##
 ########################################################
 #Set Timezone
-& "$env:windir\System32\tzutil.exe /s "W. Europe Standard Time"
+& $env:windir\System32\tzutil.exe /s "W. Europe Standard Time"
 Write-Host "Timezone set to W. Europe Standard Time"
 
 #Set Firewall
