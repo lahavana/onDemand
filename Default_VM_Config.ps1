9b6584f828a0581073e91342bbc34153947395dd
lahavana <36070872+lahavana@users.noreply.github.com>
2026-01-13 12:36:45 +0100

    Update Default_VM_Config.ps1

--- Default_VM_Config.ps1
+++ Default_VM_Config.ps1
@@ -2,8 +2,12 @@
 ## Default VM Settings                                ##
 ########################################################
 #Set Timezone
-& $env:windir\System32\tzutil.exe /s "W. Europe Standard Time"
-Write-Host "Timezone set to W. Europe Standard Time"
+#& $env:windir\System32\tzutil.exe /s "W. Europe Standard Time"
+#Write-Host "Timezone set to W. Europe Standard Time"
+
+
+$TZ = Get-TimeZone -ListAvailable | Select-Object -ExpandProperty Id | Where-Object { $_ -like '*W. Europe*' }
+Set-TimeZone -Id $TZ
 
 #Set Firewall
 Set-NetFirewallProfile -Profile Domain -Enabled false
